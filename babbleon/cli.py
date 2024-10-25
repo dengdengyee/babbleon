529e4e827bdac09a2453909f9ed5363363265b48
mh <akic4op4@gmail.com>
2024-10-25 20:22:23 +0900

    Add generate command

--- babbleon/cli.py
+++ babbleon/cli.py
@@ -1,9 +1,11 @@
 import click
 import json
+import toml
 from pathlib import Path
-from importlib import resources
+from importlib_resources import files
 
 from .config import BabbleonConfig
+from .translate import BabbleonTranslator
 
 config_file = Path("babbleon.json")
 
@@ -11,12 +13,17 @@ config_file = Path("babbleon.json")
 @click.group()
 @click.pass_context
 def cli(ctx):
-    ctx.obj = BabbleonConfig(config_file)
+    # if command is not init, check if config file exists
+    if ctx.invoked_subcommand != "init":
+        if not config_file.exists():
+            raise click.UsageError(
+                "babbleon.json not found. Please run `babbleon init` first."
+            )
+        ctx.obj = BabbleonConfig(config_file)
 
 
 @cli.command()
-@click.pass_context
-def init(ctx):
+def init():
     """Initialize a new project"""
 
     # Check if file already exists
@@ -28,7 +35,8 @@ def init(ctx):
             return
 
     # Read template using importlib.resources
-    with resources.open_text("babbleon", "config-template.json") as src:
+    template_path = files("babbleon.templates").joinpath("babbleon.json")
+    with open(template_path, "r", encoding="utf-8") as src:
         config_content = src.read()
 
     with open(config_file, "w", encoding="utf-8") as dest:
@@ -36,8 +44,11 @@ def init(ctx):
 
     click.echo("Created babbleon.json with sample configuration")
 
-    reference_file = Path(ctx.obj.get_reference_file())
-    with resources.open_text("babbleon", "reference-template.toml") as src:
+    config = BabbleonConfig(config_file)
+
+    reference_file = Path(config.get_reference_file())
+    reference_path = files("babbleon.templates").joinpath("reference.toml")
+    with open(reference_path, "r", encoding="utf-8") as src:
         reference_content = src.read()
 
     if not reference_file.exists():
@@ -46,6 +57,23 @@ def init(ctx):
         dest.write(reference_content)
 
     click.echo("Created reference-template.toml with sample text")
+
+
+@cli.command()
+@click.pass_context
+def generate(ctx):
+    """Generate the translation files for the target languages"""
+    config = ctx.obj
+    reference_file_content = config.get_reference_file().read_text(encoding="utf-8")
+    translator = BabbleonTranslator(reference_file_content)
+    output_dir = config.get_output_dir()
+    output_dir.mkdir(parents=True, exist_ok=True)
+    for language in config.get_target_languages():
+        print(f"Translating to {language}")
+        local_json_string = translator.translate(language)
+        local_json_file = output_dir.joinpath(f"{language}.json")
+        with open(local_json_file, "w", encoding="utf-8") as dest:
+            dest.write(local_json_string)
 
 
 if __name__ == "__main__":
